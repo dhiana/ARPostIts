73c2e4c6e73e98b8d5df89e0f2c16bda0b2c3d16
Dhiana Deva <dhiana.deva@gmail.com>
2014-05-13 01:12:41 -0300

    Copies Vuforia sample project

--- README.md
+++ README.md
@@ -2,3 +2,12 @@ ARPostIts
 =========
 
 Post-its gone Augmented Reality!
+
+Setting up!
+-----------
+
+* Install JDK, ADT, NDT and Vuforia SDK
+* Update PATH at ~/.bashrc so it includes ADT tools and plataform tools
+* Copy Vuforia.jar and armeabi-v7a/libVuforia.so from SDK to project's libs folder
+* ant debug
+* adb install bin/ARPostIts-debug.apk
---
+++ src/com/qualcomm/vuforia/samples/VuforiaSamples/ui/SampleAppMenu/SampleAppMenuInterface.java
@@ -0,0 +1,13 @@
+/*==============================================================================
+ Copyright (c) 2012-2014 Qualcomm Connected Experiences, Inc.
+ All Rights Reserved.
+ ==============================================================================*/
+
+package com.qualcomm.vuforia.samples.VuforiaSamples.ui.SampleAppMenu;
+
+// To be implemented in the activities to handle any desired function 
+// previously set for a SampleAppMenu instance
+public interface SampleAppMenuInterface
+{
+    public boolean menuProcess(int command);
+}
